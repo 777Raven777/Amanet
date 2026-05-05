cedc8bafa7830f9ec9665bf8a422c324bf741092
StalkerAVG <artem.kmiet@gmail.com>
2026-05-05 17:27:28 +0200

    Added waiting requests list for friends system

---
+++ backend/Models/DTO/RelationshipDTOs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Http;
+
+namespace backend.Models.DTO
+{
+    public class RelationshipDTO
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public UserDTO Sender { get; set; }
+
+        [Required]
+        public UserDTO Receiver { get; set; }
+
+        [Required]
+        public RelationshipType Status { get; set; }
+    }
+
+    public class WaitingRelationshipsDTO
+    {
+        [Required]
+        public List<RelationshipDTO> Sent { get; set; }
+        [Required]
+        public List<RelationshipDTO> Received { get; set; }
+    }
+}
