9a146b2ebbcce721ec80f5da078ae55c00b5c4c4
7Kruk7 <adamkrusze2@gmail.com>
2026-05-13 15:13:38 +0200

    added cache

---
+++ backend/Cache/RedisCacheService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers.Cache;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+using System.Text.Json;
+
+
+namespace backend.Cache;
+    
+public class RedisCacheService : ICacheService
+{
+    private readonly IDistributedCache _cache;
+
+    public RedisCacheService (IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task SetAsync<T>(string key, T value, TimeSpan? ttl = null)
+    {
+        var expiration = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = ttl ?? TimeSpan.FromMinutes(5)
+        };
+
+        var jsonData = JsonSerializer.Serialize(value);
+        await _cache.SetStringAsync(key, jsonData, expiration);
+    }
+
+    public async Task<T?> GetAsync<T> (string key)
+    {
+        var jsonData = await _cache.GetStringAsync(key);
+        return jsonData == null ? default : JsonSerializer.Deserialize<T>(jsonData);
+    }
+
+    public async Task RemoveAsync(string key)
+    {
+        await _cache.RemoveAsync(key);
+    }
+}
